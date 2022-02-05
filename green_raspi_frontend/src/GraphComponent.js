0cb1c5e74ea5608e368635030515031ab8f43529
Gabe <afoste53@gmail.com>
2022-02-04 17:28:20 -0700

    add graph component

---
+++ green_raspi_frontend/src/GraphComponent.js
@@ -0,0 +1,48 @@
+/** @format */
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Chart } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const GraphComponent = ({ x, y, scale = 1 }) => {
+  return (
+    <div className="graph-container">
+      <Line
+        datasetIdKey="id"
+        data={{
+          labels: ["Jun", "Jul", "Aug"],
+          datasets: [
+            {
+              id: 1,
+              label: "",
+              data: [5, 6, 7],
+            },
+            {
+              id: 2,
+              label: "",
+              data: [3, 2, 1],
+            },
+          ],
+        }}
+      />
+    </div>
+  );
+};
--- green_raspi_frontend/src/ValuesScreen.js
+++ green_raspi_frontend/src/ValuesScreen.js
@@ -1,26 +1,32 @@
 /** @format */
 import { Container } from "react-bootstrap";
+import { GraphComponent } from "./GraphComponent";
 
 export const ValueScreen = ({
   vals: { t_pressure, t_humidity, humidity, ph },
 }) => {
   return (
-    <Container className="app-container">
-      <h1>Current Values</h1>
-      <div>
-        <p>
-          <strong>Temp (Pressure)</strong>: {t_pressure}
-        </p>
-        <p>
-          <strong>Temp (Humidity)</strong>: {t_humidity}
-        </p>
-        <p>
-          <strong>Humidity</strong>: {humidity}
-        </p>
-        <p>
-          <strong>Ph</strong>: {ph}
-        </p>
-      </div>
+    <Container className="d-flex flex-column" id="outer-container">
+      <Container className="app-container">
+        <h1>Current Values</h1>
+        <div>
+          <p>
+            <strong>Temp (Pressure)</strong>: {t_pressure}
+          </p>
+          <p>
+            <strong>Temp (Humidity)</strong>: {t_humidity}
+          </p>
+          <p>
+            <strong>Humidity</strong>: {humidity}
+          </p>
+          <p>
+            <strong>Ph</strong>: {ph}
+          </p>
+        </div>
+      </Container>
+      <Container className="app-container">
+        <GraphComponent x={2} y={2} />
+      </Container>
     </Container>
   );
 };
